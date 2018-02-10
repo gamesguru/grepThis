7e57bd6ae24cbc3cd5d23a27e4086db9b666f27d
gamesguru <mathmuncher11@gmail.com>
2018-02-09 21:45:00 -0500

    added interrupts

--- Program.cs
+++ Program.cs
@@ -12,78 +12,103 @@ namespace grepThis
         static string query;
         static string filQuery = "matches.TXT";
         static List<string> matchingFiles;
+        static bool suspended;
         public static void Main(string[] args)
         {
             settings();
             while (true)
-            {
-            	matchingFiles = new List<string>();
+            {                
+                matchingFiles = new List<string>();
                 print("grep this: ");
                 query = Console.ReadLine();
-                filQuery= $"{root}{slash}matches.TXT";
+                filQuery = $"{root}{slash}matches.TXT";
                 //filQuery = "";
                 if (dontPrint)
                     File.AppendAllText(filQuery, $"grep this: {query}\n");
                 //for (int i = 0; i < query.Length; i++)
-                    //if (Char.IsLetterOrDigit(query[i]))
-                    //    filQuery += query[i];
-                    //else
-                        //filQuery += "";
+                //if (Char.IsLetterOrDigit(query[i]))
+                //    filQuery += query[i];
+                //else
+                //filQuery += "";
                 if (String.IsNullOrEmpty(query))
                     try { throw new ArgumentException("the string to find may not be empty", "query"); }
                     catch (Exception e) { print(e.ToString() + "\n"); continue; }
                 foreach (string file in Directory.GetFiles(root, "*.*", SearchOption.AllDirectories))
                 {
+                top:
+                    if (Console.KeyAvailable)
+                    {
+                        ConsoleKeyInfo cki = Console.ReadKey();
+                        if (cki.Modifiers == ConsoleModifiers.Shift && cki.Key == ConsoleKey.Q)
+                            break;
+                        else if (cki.Key == ConsoleKey.Backspace)
+                            suspended = true;
+                        else if (cki.Key == ConsoleKey.Spacebar)
+                            suspended = false;
+                        else if (cki.Modifiers == ConsoleModifiers.Shift && cki.Key == ConsoleKey.P)
+                            dontPrint = !dontPrint;
+                    }
+                    while (suspended)
+                    {
+                        System.Threading.Thread.Sleep(80);
+                        goto top;
+                    }
                     string[] lines = File.ReadAllLines(file);
-                    for (int i = 0; i < lines.Length; i++)                    
+                    for (int i = 0; i < lines.Length; i++)
                         if (lines[i].ToUpper().Contains(query.ToUpper()))
-                                printMatch(file, i, lines[i], query);                    
+                        {
+	    					printMatch(file, i, lines[i], query);
+                            if (dontPrint)
+                                break;
+                        }
                 }
-                if (dontPrint)
-                {
-					println($"\nfound: {matchingFiles.Count} matching files!");
-                    File.AppendAllText(filQuery, $"\nfound: {matchingFiles.Count} matching files!\n\n");
-                }
+                println($"\rfound: {matchingFiles.Count} matching files!!");
+                File.AppendAllText(filQuery, $"\nfound: {matchingFiles.Count} matching files!\n\n");
+
                 println("\n");
             }
         }
 
         private static void printMatch(string filename, int lineNumber, string line, string query)
         {
-        	if (dontPrint)
-        	{
-                if (filename == filQuery)
-                    return;
-        		if (!matchingFiles.Contains(filename))
-        		{
-        			string str = $"{filename}::Ln{lineNumber}";
-        			matchingFiles.Add(filename);
-                    File.AppendAllText(filQuery, $"{str}\n");
-        		}
-        		return;
-        	}
-            print(filename.Replace(root, ""), ConsoleColor.DarkMagenta);
-            print(":", ConsoleColor.Blue);
-            print(lineNumber.ToString(), ConsoleColor.Green);
-            print(":", ConsoleColor.Blue);
+            if (filename == filQuery || filename == "settings.py")
+                return;
 
-            List<int> insts = AllIndexesOf(line.ToUpper(), query.ToUpper());
+            if (!matchingFiles.Contains(filename))
+            {
+                string str = $"{filename}::Ln{lineNumber}";
+                matchingFiles.Add(filename);
+                File.AppendAllText(filQuery, $"{str}\n");
+            }
+
+            if (!dontPrint)
+            {
+                print(filename.Replace(root, ""), ConsoleColor.DarkMagenta);
+                print(":", ConsoleColor.Blue);
+                print(lineNumber.ToString(), ConsoleColor.Green);
+                print(":", ConsoleColor.Blue);
+
+                List<int> insts = AllIndexesOf(line.ToUpper(), query.ToUpper());
+
+                print(line.Substring(0, insts[0]));
+                print(line.Substring(insts[0], query.Length), ConsoleColor.DarkRed);
+                for (int i = 0; i < insts.Count; i++)
+                    try
+                    {
+                        print(line.Substring(insts[i] + query.Length, insts[i + 1] - insts[i] - query.Length));
+                        print(line.Substring(insts[i + 1], query.Length), ConsoleColor.DarkRed);
+                    }
+                    catch
+                    {
+                        print(line.Substring(insts[i] + query.Length));
+                    }
+
+                println();
+            }
+            else
+				print($"\rfound: {matchingFiles.Count} matching files");
             
-            print(line.Substring(0, insts[0]));
-            print(line.Substring(insts[0], query.Length), ConsoleColor.DarkRed);           
-            for (int i = 0; i < insts.Count; i++)            
-                try
-                {
-                    print(line.Substring(insts[i] + query.Length, insts[i + 1] - insts[i] - query.Length));
-                    print(line.Substring(insts[i + 1], query.Length), ConsoleColor.DarkRed);
-                }
-                catch
-                {
-                    print(line.Substring(insts[i] + query.Length));
-                }
-            
-            println();
-        }        
+        } 
 
         public static List<int> AllIndexesOf(this string str, string value)
         {
--- bin/Debug/settings.py
+++ bin/Debug/settings.py
@@ -1,8 +1,21 @@
+##############
+## CONTROLS ##
+##############
+## Backspace = Pause
+## Spacebar  = Resume
+## Ctrl + Q  = Abort
+## Ctrl + P  = Toggle print (it always saves a list of matches, limited to the first line number match of that file)
+#  It is case insensitive without much option for regex, it searches as is.
+
+
+
+## !!SETTINGS DO NOT WORK YET, only dontPrint!!
+#
 ##############################
 ## Configurable Directories ##
 ##############################
+[dontPrint]	true
 [dir]		$root
 [dir]		C:/users/administrator/desktop/ocast_newest/bin/debug/logs
 [exclude]	*.XML
 [priority]	*.TXT
-[dontPrint]	true
