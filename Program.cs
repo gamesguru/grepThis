27c0f364171b885afa3112f10f53b8751df52f6c
gamesguru <mathmuncher11@gmail.com>
2018-02-10 19:06:10 -0500

    added dirs

--- Program.cs
+++ Program.cs
@@ -9,63 +9,64 @@ namespace grepThis
         static string root = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
         static string sl = Path.DirectorySeparatorChar.ToString();
         static string nl = Environment.NewLine;
+        //settings
         static bool dontPrint;
+        static List<string> dirs = new List<string>();
+        static List<string> chosenDirs = new List<string>();
+        static bool[] _checked;
+        //search
         static string query;
-        static string filQuery = "matches.TXT";
+        static string matches;
         static List<string> matchingFiles;
         static bool suspended;
         public static void Main(string[] args)
         {
             settings();
+            chooseDirs();
             printControls();
             while (true)
-            {                
+            {
                 matchingFiles = new List<string>();
                 print("grep this: ");
                 query = Console.ReadLine();
-                filQuery = $"{root}{sl}matches.TXT";
-                //filQuery = "";
+                matches = $"{root}{sl}matches.TXT";
                 if (dontPrint)
-                    File.AppendAllText(filQuery, $"grep this: {query}\n");
-                //for (int i = 0; i < query.Length; i++)
-                //if (Char.IsLetterOrDigit(query[i]))
-                //    filQuery += query[i];
-                //else
-                //filQuery += "";
+                    File.AppendAllText(matches, $"grep this: {query}\n");
                 if (String.IsNullOrEmpty(query))
                     try { throw new ArgumentException("the string to find may not be empty", "query"); }
                     catch (Exception e) { print(e.ToString() + "\n"); continue; }
-                foreach (string file in Directory.GetFiles(root, "*.*", SearchOption.AllDirectories))
-                {
-                    if (file == filQuery || file.EndsWith("settings.py"))
-                        continue;
-                    top:
-                    if (Console.KeyAvailable)
+                foreach (string directory in chosenDirs)
+                    foreach (string file in Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories))
                     {
-                        ConsoleKeyInfo cki = Console.ReadKey();
-                        if (cki.Modifiers == ConsoleModifiers.Shift && cki.Key == ConsoleKey.Q)
-                            break;
-                        else if (cki.Key == ConsoleKey.Backspace)
-                            suspended = true;
-                        else if (cki.Key == ConsoleKey.Spacebar)
-                            suspended = false;
-                        else if (cki.Modifiers == ConsoleModifiers.Shift && cki.Key == ConsoleKey.P)
-                            dontPrint = !dontPrint;
-                    }
-                    while (suspended)
-                    {
-                        System.Threading.Thread.Sleep(80);
-                        goto top;
-                    }
-                    string[] lines = File.ReadAllLines(file);
-                    for (int i = 0; i < lines.Length; i++)
-                        if (lines[i].ToUpper().Contains(query.ToUpper()))
+                        if (file == matches || file == $"{root}{sl}settings.py")
+                            continue;
+                        top:
+                        if (Console.KeyAvailable)
                         {
-                            printMatch(file, i, lines[i], query);
-                            if (dontPrint)
+                            ConsoleKeyInfo cki = Console.ReadKey();
+                            if (cki.Key == ConsoleKey.Escape)
                                 break;
+                            else if (cki.Key == ConsoleKey.Backspace)
+                                suspended = true;
+                            else if (cki.Key == ConsoleKey.Spacebar)
+                                suspended = false;
+                            else if (cki.Key == ConsoleKey.Insert || cki.Key == ConsoleKey.F2)
+                                dontPrint = !dontPrint;
                         }
-                }
+                        while (suspended)
+                        {
+                            System.Threading.Thread.Sleep(80);
+                            goto top;
+                        }
+                        string[] lines = File.ReadAllLines(file);
+                        for (int i = 0; i < lines.Length; i++)
+                            if (lines[i].ToUpper().Contains(query.ToUpper()))
+                            {
+                                printMatch(file, directory, i, lines[i], query);
+                                if (dontPrint)
+                                    break;
+                            }
+                    }
                 if (dontPrint)
                 {
 					foreach (string s in matchingFiles)
@@ -74,24 +75,24 @@ namespace grepThis
                 }
                 else
                     println($"\rfound: {matchingFiles.Count} matching files!!");
-                File.AppendAllText(filQuery, $"\nfound: {matchingFiles.Count} matching files!\n\n");
+                File.AppendAllText(matches, $"\nfound: {matchingFiles.Count} matching files!\n\n");
 
                 println("\n");
             }
         }
 
-        static void printMatch(string filename, int lineNumber, string line, string query)
+        static void printMatch(string filename, string directory, int lineNumber, string line, string query)
         {
             if (!matchingFiles.Contains(filename))
             {
                 string str = $"{filename}::Ln{lineNumber}";
                 matchingFiles.Add(filename);
-                File.AppendAllText(filQuery, $"{str}\n");
+                File.AppendAllText(matches, $"{str}\n");
             }
 
             if (!dontPrint)
             {
-                print(filename.Replace(root, ""), ConsoleColor.DarkMagenta);
+                print(filename.Replace(directory, ""), ConsoleColor.DarkMagenta);
                 print(":", ConsoleColor.Blue);
                 print(lineNumber.ToString(), ConsoleColor.Green);
                 print(":", ConsoleColor.Blue);
@@ -109,6 +110,7 @@ namespace grepThis
                     catch
                     {
                         print(line.Substring(insts[i] + query.Length));
+                        break; //new
                     }
 
                 println();
@@ -149,18 +151,83 @@ namespace grepThis
         	foreach (string s in File.ReadAllLines($"{root}{sl}settings.py"))
         	{
         		st = s.Replace("\t", "").Trim();
-        		if (st.StartsWith("[dontPrint]"))
-        			dontPrint= Convert.ToBoolean(st.Replace("[dontPrint]", ""));
+                if (st.StartsWith("[dontPrint]"))
+                    dontPrint = Convert.ToBoolean(st.Replace("[dontPrint]", ""));
+                else if (st.StartsWith("[dir]") && Directory.Exists(st.Replace("[dir]", "").Replace("$root", root)))
+                    dirs.Add(st.Replace("[dir]", "").Replace("$root", root));
         	}
         }
+
+        static void chooseDirs()
+        {
+            _checked = new bool[dirs.Count];
+            for (int i = 0; i < _checked.Length; i++)
+                _checked[i] = true;
+            if (dirs.Count == 0)
+            {
+                print("ERROR: ", ConsoleColor.DarkRed);
+                println("no directories configured, add lines to settings.py: [dir] <dir_path>");
+                println("press any key to exit...");
+                Console.ReadKey();
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+            else if (dirs.Count == 1)
+            {
+				chosenDirs.Add(dirs[0]);
+                return;        
+            }
+
+            int max = 0;
+            foreach (string s in dirs)
+                if (Directory.Exists(s) && s.Length > max)
+                    max = s.Length;
+            string[] pad = new string[dirs.Count];
+            for (int i = 0; i < dirs.Count; i++)
+                    for (int j = max; j > dirs[i].Length; j--)
+                        pad[i] += " ";
+
+            while (true)
+            {
+                for (int i = 0; i < dirs.Count; i++)
+                {
+                    string str = _checked[i] ? "x" : " ";
+                    println($"#{i + 1}: {dirs[i]}{pad[i]}[{str}]");
+                }
+                print("\nMake a selection, press enter to continue: ");
+                string s = Console.ReadLine();
+                if (s == "")
+                    break;
+                int j = -1;
+                try { j = Convert.ToInt32(s); }
+                catch (Exception e)
+                {
+                    print($"ERROR: ", ConsoleColor.DarkRed);
+                    println(e.Message);
+                    println();
+                    continue;
+                }
+                if (j > 0 && j <= dirs.Count)
+                    _checked[j - 1] = !_checked[j - 1];
+                else
+                {
+                    print($"ERROR: ", ConsoleColor.DarkRed);
+                    println($"value must be between 1 and {dirs.Count}");
+                }
+                println("\nNew choices:");
+            }
+            for (int i = 0; i < dirs.Count; i++)
+                if (_checked[i])
+                    chosenDirs.Add(dirs[i]);
+        }
+
         static void printControls()
         {
             println("Welcome to grep for windows, by Shane");
             println($"{nl}##############{nl}## CONTROLS ##{nl}##############");
             println("## Backspace = Pause");
             println("## Spacebar  = Resume");
-            println("## Shift + Q  = Abort");
-            println("## Shift + P  = Toggle print (it always saves a list of matches, limited to the first line number match of that file)");
+            println("## Escape  = Abort");
+            println("## Insert  = Toggle print (it always saves a list of matches, limited to the first line number match of that file) [F2 == alt control]");
             println($"## It is case insensitive without much option for regex, it searches as is.{nl}");
         }
     }
--- bin/Debug/settings.py
+++ bin/Debug/settings.py
@@ -3,19 +3,20 @@
 ##############
 ## Backspace = Pause
 ## Spacebar  = Resume
-## Shift + Q  = Abort
-## Shift + P  = Toggle print (it always saves a list of matches, limited to the first line number match of that file)
+## Escape  = Abort
+## Insert  = Toggle print (it always saves a list of matches, limited to the first line number match of that file) [F2 is an alternative key!!]
 #  It is case insensitive without much option for regex, it searches as is.
 
 
 
-## !!SETTINGS DO NOT WORK YET, only dontPrint!!
-#
 ##############################
 ## Configurable Directories ##
 ##############################
-[dontPrint]	true
-[dir]		$root
-[dir]		C:/users/administrator/desktop/ocast_newest/bin/debug/logs
-[exclude]	*.XML
-[priority]	*.TXT
+[dontPrint]	false
+#[dir]		$root
+[dir]		/media/shane/RaptorWS/ocLogs
+#[dir]		C:/users/administrator/desktop/ocast_newest/bin/debug/logs
+
+#non-functional at the moment
+#[exclude]	*.XML
+#[priority]	*.TXT
